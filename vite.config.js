c93c889bc64bf128063d2970a5a54362a718e4f9
jithuth <tjithu1992@gmail.com>
2026-02-04 00:52:18 +0300

    chore: Update Vite build output directory from 'build' to 'dist'.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is the critical fix for your 404 errors
-  base: '/school/',
+
   build: {
     // This ensures your build files go into a folder 
     // that Django can easily find
